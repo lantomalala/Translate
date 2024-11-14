bf78fcf976649c5586346e085bab8cbcc3c1e771
lantomalala <3574@corp.smartone-group.com>
2024-11-14 09:44:29 +0300

    Update file

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function Component() {
         <h1 style={{ textAlign: 'center' ,marginBottom : 100, color: "white"}}>Vozona Gasy</h1>
           <Form onSubmit={(e) => e.preventDefault()} className="d-flex gap-2">
             <div className="position-relative flex-grow-1">
-              <Form.Control type="text" placeholder="Entrez un texte à traduire" value={inputText} onChange={(e) => setInputText(e.target.value)} className='formControl' />
+              <Form.Control as="textarea" placeholder="Entrez un texte à traduire" value={inputText} onChange={(e) => { setInputText(e.target.value); e.target.style.height = "auto";  e.target.style.height = `${e.target.scrollHeight}px`; }} className="formControl" />
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-search position-absolute" viewBox="0 0 16 16" style={{ left: '16px', top: '50%', transform: 'translateY(-50%)', color: '#6c757d' }}>
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
               </svg>
